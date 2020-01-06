b9e6b722c5e9a79ae8254c47edbf064d84d3727d
Ville Skyttä <ville.skytta@iki.fi>
2020-01-06 20:45:25 +0200

    *: add git and emacs settings

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode
+  (fill-column . 88)
+  (eval add-hook 'before-save-hook 'blacken-buffer nil t)))
+
